04914424fe3038eb353093353f270d61bd19ddf9
query <15927700473@163.com>
2018-05-01 14:47:38 +0800

    优化文件上传

--- demos/web/04.js
+++ demos/web/04.js
@@ -16,27 +16,31 @@ app.use(staticServer(path.resolve(__dirname, 'public')));
 app.use(koaBody({ multipart: true }));
 
 const upload = async ctx => {
-  const files = ctx.request.body.files;
-
+  const body = ctx.request.body.files;
+  console.log(body); // { upfile: [文件列表]}
+  console.log(ctx.request.body);
   const filePaths = [];
 
-  for (let k in files) {
-    const file = files[k]; // file是数组
-    const filePath = path.resolve(__dirname, 'uploads');
-    for (let i = 0; i < file.length; i++) {
-      const filePath = path.resolve(__dirname, 'uploads', file[i].name);
-      const reader = fs.createReadStream(file[i].path); // file.path：文件上传的临时位置
-      const writer = fs.createWriteStream(filePath);
+  for (let k in body) {
+    const files = body[k]; // file是数组
+    files.forEach(file => {
+      const ext = path.extname(file.name);
+      const basename = path.basename(file.path);
 
+      const fileDestPath = path.resolve(__dirname, 'uploads', `${basename}${ext}`);
+
+      const reader = fs.createReadStream(file.path);
+      const writer = fs.createWriteStream(fileDestPath);
       reader.pipe(writer);
-      filePaths.push(filePath);
-    }
+
+      filePaths.push(fileDestPath);
+    });
   }
   ctx.body = filePaths;
 };
 
 app.use(route.post('/upload', upload));
 
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log('running...');
 });
